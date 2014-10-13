f90c1ce8e4f52c652bb5aed4f319e719f9b1a1d7
pmpod <piotr.podziemski@gmail.com>
2014-10-13 14:30:37 +0200

    Minor Adjustment for Oscillator class (updated getUniformTimestepCurrentSource)

--- Model/Oscillator.cpp
+++ Model/Oscillator.cpp
@@ -174,7 +174,7 @@ double Oscillator::getUniformTimestepCurrentSource()
 	double Vinterpol = 0;
 	for (unsigned int i = 0; i < m_neighbours.size(); ++i)
 	{
-		if (m_currentTime == (m_neighbours[i]->getCurrentTime()))
+		if (m_currentTime != (m_neighbours[i]->getCurrentTime()))
 			Vinterpol = m_neighbours[i]->m_v_potential;
 		else
 			Vinterpol = m_neighbours[i]->getPreviousPotential();
@@ -186,8 +186,8 @@ double Oscillator::getUniformTimestepCurrentSource()
 	return m_currentSource;
 }
 //--------------------------------------------------------------
-double Oscillator::getLastCurrentSource(){	return m_currentSource;}
-
+double Oscillator::getLastCurrentSource(){ return m_currentSource; }
+//--------------------------------------------------------------
 int Oscillator::getNumberOfCurrents(){ return this->m_v_current.size(); }
 //---------------------------------------------------------------
 void Oscillator::setPositionX(const double& pos)
