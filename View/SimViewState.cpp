3db583caf422dae20513f6d2f34bff81b9cca610
pmpod <piotr.podziemski@gmail.com>
2014-10-26 21:12:36 +0100

    Addted view State class for state pattern for view/painting handling

---
+++ View/SimViewState.cpp
@@ -0,0 +1,11 @@
+#include "SimViewState.h"
+
+
+SimViewState::SimViewState()
+{
+}
+
+
+SimViewState::~SimViewState()
+{
+}
---
+++ View/SimViewState.h
@@ -0,0 +1,8 @@
+#pragma once
+class SimViewState
+{
+public:
+	SimViewState();
+	~SimViewState();
+};
+
