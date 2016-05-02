4ca354ad9069ce86106db768921b0230a6442fc1
pp@devon <erlenstar@wp.pl>
2016-05-02 12:33:17 +0200

    Grid electrode is working and saving results to mat file

--- GeneratedFiles/Release/moc_GridElectrode.cpp
+++ GeneratedFiles/Release/moc_GridElectrode.cpp
@@ -1,7 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'GridElectrode.h'
 **
-** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.6)
+** Created: Fri Apr 22 00:17:50 2016
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -9,8 +10,8 @@
 #include "../../GridElectrode.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'GridElectrode.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.6. It"
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.7.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -19,7 +20,7 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_GridElectrode[] = {
 
  // content:
-       6,       // revision
+       5,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
@@ -36,21 +37,9 @@ static const char qt_meta_stringdata_GridElectrode[] = {
     "GridElectrode\0"
 };
 
-void GridElectrode::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
-{
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
-}
-
-const QMetaObjectExtraData GridElectrode::staticMetaObjectExtraData = {
-    0,  qt_static_metacall 
-};
-
 const QMetaObject GridElectrode::staticMetaObject = {
     { &QObject::staticMetaObject, qt_meta_stringdata_GridElectrode,
-      qt_meta_data_GridElectrode, &staticMetaObjectExtraData }
+      qt_meta_data_GridElectrode, 0 }
 };
 
 #ifdef Q_NO_DATA_RELOCATION
